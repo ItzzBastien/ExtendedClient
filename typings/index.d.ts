dc5f7a83492fa44087e27030622ba49a89c4bbd9
Thekillerazert <hutin.bastien973@gmail.com>
2022-05-09 15:00:41 -0300

    master

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,3 +1,4 @@
+declare module "extended-client";
 declare const ExtendedClient: typeof import("./Structures/Extended.js");
 declare const Builder: typeof import("./Structures/Builder.js");
 export { ExtendedClient, Builder };
