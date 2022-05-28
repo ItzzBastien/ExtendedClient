403a0d044d2ef5b9cb1b76ca8c25eed4bd79e79d
Thekillerazert <hutin.bastien973@gmail.com>
2022-05-28 17:08:46 -0300

    extended fix

--- typings/Structures/Extended.d.ts
+++ typings/Structures/Extended.d.ts
@@ -1,12 +1,8 @@
 import { Client, ClientOptions } from "discord.js";
 import { EventHandler } from "../Handlers/EventHandler.js";
 import { SlashCommandHandler } from "../Handlers/SlashHandler.js";
-export interface ExtendedClientParam extends ClientOptions {
-    slashCommandHandler?: boolean;
-    eventHandler?: boolean;
-}
 export declare class ExtendedClient extends Client {
     slashCommands: SlashCommandHandler;
     eventHandler: EventHandler;
-    constructor(params: ExtendedClientParam);
+    constructor(params: ClientOptions);
 }
