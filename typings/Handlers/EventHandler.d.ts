18f7535accf3aad16ff1cb9f6509634500cb4db7
Thekillerazert <hutin.bastien973@gmail.com>
2022-05-28 17:00:13 -0300

    typed event file

--- typings/Handlers/EventHandler.d.ts
+++ typings/Handlers/EventHandler.d.ts
@@ -1,4 +1,10 @@
+import { ClientEvents } from "discord.js";
 import { ExtendedClient } from "../Structures/Extended.js";
+export interface EventFile {
+    name: keyof ClientEvents;
+    once: boolean;
+    execute: (client: ExtendedClient, ...args: any[]) => any | Promise<any>;
+}
 export declare class EventHandler {
     private client;
     constructor(client: ExtendedClient);
