30fd1868550d51e20cebbf8a7d83ecda724dc867
Thekillerazert <hutin.bastien973@gmail.com>
2022-05-28 17:25:45 -0300

    auto login

--- typings/Structures/Extended.d.ts
+++ typings/Structures/Extended.d.ts
@@ -1,8 +1,12 @@
 import { Client, ClientOptions } from "discord.js";
 import { EventHandler } from "../Handlers/EventHandler.js";
 import { SlashCommandHandler } from "../Handlers/SlashHandler.js";
+interface ExtendedClientOptions extends ClientOptions {
+    token: string;
+}
 export declare class ExtendedClient extends Client {
     slashCommands: SlashCommandHandler;
     eventHandler: EventHandler;
-    constructor(params: ClientOptions);
+    constructor(params: ExtendedClientOptions);
 }
+export {};
